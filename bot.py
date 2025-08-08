725e8c528ad22e8ccf221f678b550eab6160bf94
Вячеслав Евгеньевич Бочкарев <70199356+slavabochkarev@users.noreply.github.com>
2025-08-08 15:59:46 +0500

    Fix path

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import os
 import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from dotenv import load_dotenv
 import requests
 from telegram import Update, ReplyKeyboardMarkup, KeyboardButton
